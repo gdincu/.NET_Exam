2ee6fa1c218504ee6f66bda4b96ee4147e0ce1cf
gdincu <dici0137@scs.ubbcluj.ro>
2020-06-26 00:37:56 +0300

    20200626 Added Swagger

--- Domain/ModelValidators/Startup.cs
+++ Domain/ModelValidators/Startup.cs
@@ -13,6 +13,9 @@ using FluentValidation;
 using System.Text.Json.Serialization;
 using FluentValidation.AspNetCore;
 using System.Reflection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
 
 namespace Exam
 {
@@ -25,11 +28,14 @@ namespace Exam
 
         public IConfiguration Configuration { get; }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("Bookings")));
             //services.AddControllersWithViews();
+
             services
                 .AddControllers()
                 .AddJsonOptions(options =>
@@ -38,11 +44,27 @@ namespace Exam
                     options.JsonSerializerOptions.IgnoreNullValues = true;
                 })
                 .AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
-
+                
             services.AddTransient<IValidator<Booking>, BookingValidator>()
-            .AddTransient<IValidator<User>, UserValidator>()
-            .AddTransient<IValidator<Location>, LocationValidator>()
-            .AddTransient<IValidator<Comment>, CommentValidator>();
+                .AddTransient<IValidator<User>, UserValidator>()
+                .AddTransient<IValidator<Location>, LocationValidator>()
+                .AddTransient<IValidator<Comment>, CommentValidator>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      /*builder.WithOrigins("http://localhost:4200/")
+                                      .AllowAnyHeader()
+                                      .AllowAnyMethod()
+                                      .AllowAnyOrigin();*/
+
+                                      builder.AllowAnyOrigin()
+                                      .AllowAnyHeader()
+                                      .AllowAnyMethod();
+                                  });
+            });
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -50,11 +72,49 @@ namespace Exam
                 configuration.RootPath = "ClientApp/dist";
             });
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Exam API",
+                    Description = "Exam ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://github.com/gdincu"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "ID",
+                        Email = "id@id.com"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Exam API V1");
+                //c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -68,6 +128,7 @@ namespace Exam
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCors(MyAllowSpecificOrigins);
             if (!env.IsDevelopment())
             {
                 app.UseSpaStaticFiles();
@@ -94,6 +155,9 @@ namespace Exam
                     spa.UseAngularCliServer(npmScript: "start");
                 }
             });
+
+            
+            
         }
     }
 }
